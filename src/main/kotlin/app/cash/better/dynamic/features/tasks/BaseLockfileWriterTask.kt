75a022a2747072a36f3f5c3b6b1841bcedaee76e
Derek Ellis <derekellis@squareup.com>
2022-12-01 13:19:32 -0500

    Migrate to using lockfiles to enforce versions
    Remove check versions task, as this is now handled by gradle itself
    Add tests for variant-awareness

---
+++ src/main/kotlin/app/cash/better/dynamic/features/tasks/BaseLockfileWriterTask.kt
@@ -0,0 +1,69 @@
+package app.cash.better.dynamic.features.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+import kotlin.io.path.div
+
+abstract class BaseLockfileWriterTask : DefaultTask() {
+  @get:InputFiles
+  abstract var partialFeatureLockfiles: List<File>
+
+  @get:InputFile
+  abstract var partialBaseLockfile: File
+
+  @get:OutputFile
+  abstract var outputLockfile: File
+
+  @TaskAction
+  fun generateLockfile() {
+    val mergedLockfileEntries = mergeLockfiles()
+
+    outputLockfile.writeText(
+      """
+      |# This is a Gradle generated file for dependency locking.
+      |# Manual edits can break the build and are not advised.
+      |# This file is expected to be part of source control.
+      |${mergedLockfileEntries.sorted().joinToString(separator = "\n")}
+      |empty=
+    """.trimMargin()
+    )
+  }
+
+  private fun mergeLockfiles(): List<LockfileEntry> {
+    project.buildDir.toPath() / "tmp"
+
+    val resolvedEntries =
+      partialBaseLockfile.readLockfileEntries().associateBy { it.artifact }
+        .toMutableMap()
+
+    val allEntries = partialFeatureLockfiles.flatMap { it.readLockfileEntries() }
+
+    allEntries.forEach { entry ->
+      val (artifact, version, configurations) = entry
+
+      val existing = resolvedEntries[artifact]
+      // We only care about the conflicting dependencies that actually exist in the base
+      if (existing != null) {
+        val resolvedVersion = maxOf(existing.version, version)
+        val resolvedConfigurations = existing.configurations + configurations
+
+        resolvedEntries[artifact] =
+          LockfileEntry(artifact, resolvedVersion, resolvedConfigurations.toSortedSet())
+      }
+    }
+
+    return resolvedEntries.values.toList()
+  }
+
+  private fun File.readLockfileEntries(): List<LockfileEntry> = readLines().map { entry ->
+    val (dependency, configurationsString) = entry.split("=")
+    val (group, artifact, version) = dependency.split(":")
+    val configurations = configurationsString.split(", ").toSortedSet()
+
+    LockfileEntry("$group:$artifact", version, configurations)
+  }
+}
