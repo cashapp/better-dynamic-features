8c62b8152b89fe916c91be19d3bbec2a968931fc
Derek Ellis <derekellis@squareup.com>
2023-02-06 12:58:54 -0500

    Use dependencyLocking.lockFile path (#44)
    Don't use hardcoded lockfile path and allow custom lockfile paths to be specified.

--- src/main/kotlin/app/cash/better/dynamic/features/tasks/CheckLockfileTask.kt
+++ src/main/kotlin/app/cash/better/dynamic/features/tasks/CheckLockfileTask.kt
@@ -1,6 +1,7 @@
 package app.cash.better.dynamic.features.tasks
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
@@ -13,14 +14,14 @@ import java.nio.file.StandardCopyOption
 
 abstract class CheckLockfileTask : DefaultTask() {
   @get:InputFile
-  abstract var newLockfile: File
+  abstract val newLockfile: RegularFileProperty
 
   @get:Internal
-  abstract var currentLockfilePath: File
+  abstract val currentLockfilePath: RegularFileProperty
 
   @get:[Optional InputFile]
   val currentLockfile: File?
-    get() = currentLockfilePath.takeIf { it.exists() }
+    get() = currentLockfilePath.get().asFile.takeIf { it.exists() }
 
   @get:OutputFile
   abstract var outputFile: File
@@ -33,10 +34,12 @@ abstract class CheckLockfileTask : DefaultTask() {
 
   @TaskAction
   fun checkLockfiles() {
-    val areTheyEqual = newLockfile.readText() == currentLockfile?.readText()
+    val newLockfileFile = newLockfile.asFile.get()
+
+    val areTheyEqual = newLockfileFile.readText() == currentLockfile?.readText()
     outputFile.writeText(areTheyEqual.toString())
     if (!areTheyEqual) {
-      Files.copy(newLockfile.toPath(), currentLockfilePath.toPath(), StandardCopyOption.REPLACE_EXISTING)
+      Files.copy(newLockfileFile.toPath(), currentLockfilePath.asFile.get().toPath(), StandardCopyOption.REPLACE_EXISTING)
       if (isCi) {
         throw IllegalStateException("The lockfile was out of date. Run the '$projectPath:writeLockfile' task and commit the updated lockfile.")
       } else {
