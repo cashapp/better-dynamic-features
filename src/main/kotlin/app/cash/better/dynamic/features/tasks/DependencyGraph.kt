9551fbfc590f1c1649531271585008375239e2b5
Derek Ellis <derekellis@squareup.com>
2023-02-10 10:33:11 -0500

    Reformat Node

--- src/main/kotlin/app/cash/better/dynamic/features/tasks/DependencyGraph.kt
+++ src/main/kotlin/app/cash/better/dynamic/features/tasks/DependencyGraph.kt
@@ -4,7 +4,13 @@ package app.cash.better.dynamic.features.tasks
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Node(val artifact: String, val version: String, val configurations: MutableSet<String>, val children: List<Node>, val isProjectModule: Boolean)
+data class Node(
+  val artifact: String,
+  val version: String,
+  val configurations: MutableSet<String>,
+  val children: List<Node>,
+  val isProjectModule: Boolean,
+)
 
 @JsonClass(generateAdapter = true)
 data class NodeList(val nodes: List<Node>)
