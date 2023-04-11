8f54cbdd069292f34404d3350264f545fd9795a6
Derek Ellis <derekellis@squareup.com>
2023-04-11 11:35:42 -0400

    Don't use delegated properties in tasks (#58)
    * Don't use delegated properties in tasks
    
    They break the configuration cache
    https://github.com/gradle/gradle/issues/23092
    
    * spotlessApply

--- gradle-plugin/src/main/kotlin/app/cash/better/dynamic/features/tasks/DependencyGraphConsumingTask.kt
+++ gradle-plugin/src/main/kotlin/app/cash/better/dynamic/features/tasks/DependencyGraphConsumingTask.kt
@@ -4,7 +4,6 @@ import com.squareup.moshi.Moshi
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Internal
 
 abstract class DependencyGraphConsumingTask : DefaultTask() {
   private val moshi = Moshi.Builder().build()
@@ -22,17 +21,11 @@ abstract class DependencyGraphConsumingTask : DefaultTask() {
   @get:InputFiles
   abstract val baseDependencyGraphFiles: ConfigurableFileCollection
 
-  @get:Internal
-  protected val baseGraph by lazy {
-    baseDependencyGraphFiles.map {
-      adapter.fromJson(it.readText())?.nodes ?: error("Could not read graph from $it")
-    }.flatten()
-  }
+  protected fun baseGraph() = baseDependencyGraphFiles.map {
+    adapter.fromJson(it.readText())?.nodes ?: error("Could not read graph from $it")
+  }.flatten()
 
-  @get:Internal
-  protected val featureGraphs by lazy {
-    featureDependencyGraphFiles.map {
-      adapter.fromJson(it.readText())?.nodes ?: error("Could not read graph from $it")
-    }
+  protected fun featureGraphs() = featureDependencyGraphFiles.map {
+    adapter.fromJson(it.readText())?.nodes ?: error("Could not read graph from $it")
   }
 }
