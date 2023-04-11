bd7fda0ce4de559d594cfbb540b6fc8a6da63fa4
Derek Ellis <derekellis@squareup.com>
2023-04-11 13:19:19 -0400

    Rework the configuration of the resource tasks
    Trying to extract the "base" module from the feature module's dependencies is flaky, so this change allows the base module to be specified explicitly.
    
    Also removes delegated properties from the resource rewriting task

---
+++ gradle-plugin/src/main/kotlin/app/cash/better/dynamic/features/BetterDynamicFeaturesFeatureExtension.kt
@@ -0,0 +1,9 @@
+// Copyright Square, Inc.
+package app.cash.better.dynamic.features
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+
+abstract class BetterDynamicFeaturesFeatureExtension {
+  abstract val baseProject: Property<Project>
+}
