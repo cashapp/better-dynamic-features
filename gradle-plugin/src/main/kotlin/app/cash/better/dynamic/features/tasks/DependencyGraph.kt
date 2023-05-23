97e1de52cb9d075ab9063a469ddc08f1d1653b49
Derek Ellis <derekellis@squareup.com>
2023-05-23 15:02:07 -0400

    Fix version comparisons in lockfile generation

--- gradle-plugin/src/main/kotlin/app/cash/better/dynamic/features/tasks/DependencyGraph.kt
+++ gradle-plugin/src/main/kotlin/app/cash/better/dynamic/features/tasks/DependencyGraph.kt
@@ -2,11 +2,12 @@
 package app.cash.better.dynamic.features.tasks
 
 import com.squareup.moshi.JsonClass
+import org.gradle.util.internal.VersionNumber
 
 @JsonClass(generateAdapter = true)
 data class Node(
   val artifact: String,
-  val version: String,
+  val version: Version,
   val variants: Set<String>,
   val children: List<Node>,
   val isProjectModule: Boolean,
@@ -20,3 +21,12 @@ enum class DependencyType {
 
 @JsonClass(generateAdapter = true)
 data class NodeList(val nodes: List<Node>)
+
+@JsonClass(generateAdapter = true)
+@JvmInline
+value class Version(val string: String) : Comparable<Version> {
+  override fun compareTo(other: Version): Int =
+    VersionNumber.parse(string).compareTo(VersionNumber.parse(other.string))
+
+  override fun toString(): String = string
+}
